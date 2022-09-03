557e9201cfc4c6b225f67fdad555305ff5ac45ac
Apeirogon99 <Apeirogon99@gmail.com>
2022-09-03 23:43:49 +0900

    [gwnaho] Add GameInstance
    게임 인스턴스 추가

---
+++ Source/MegaJam_2022_BoD/Public/Online/BoD_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "BoD_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAJAM_2022_BOD_API UBoD_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
